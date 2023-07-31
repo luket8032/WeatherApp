75bdddb6ddaddd66ed2be6024e059069cdba954d
Luke <luket8032@hotmail.com>
2023-07-31 13:43:08 -0500

    Add switch between metric and imperial measurements

--- src/dom.js
+++ src/dom.js
@@ -112,13 +112,130 @@ const dom = (() => {
         dayDate.textContent = date;
         conditionText.textContent = dayData.condition.text;
         conditionIcon.src = dayData.condition.icon;
-        loTemp.textContent = dayData.mintemp_f;
-        hiTemp.textContent = dayData.maxtemp_f;
+        loTemp.textContent = `Low: ${dayData.mintemp_f} \u00B0F`;
+        hiTemp.textContent = `High: ${dayData.maxtemp_f} \u00B0F`;
 
         dayContainer.append(dayDate, conditionText, conditionIcon, loTemp, hiTemp);
         
         return dayContainer
     }
+
+    function createForecastElementC(data) {
+        container.innerHTML = ''
+    
+        const forecastContainer = document.createElement('div');
+        const tempContainer = document.createElement('div');
+        const conditionContainer = document.createElement('div');
+        const conditionIcon = document.createElement('img');
+        const location = document.createElement('h1');
+        const localtime = document.createElement('p');
+        const tempF = document.createElement('p');
+        const condition = document.createElement('p');
+        const secondaryData = createSecondaryDataC(data);
+    
+        location.textContent = `${data.location.name}, ${data.location.country}`;
+        localtime.textContent = data.location.localtime;
+        tempF.textContent = `${data.current.temp_c} \u00B0C`;
+        condition.textContent = data.current.condition.text;
+        conditionIcon.src = data.current.condition.icon;
+    
+        conditionContainer.className = 'condition-container';
+        tempContainer.className = 'temp-container';
+        forecastContainer.className = 'forecast-container';
+    
+        conditionContainer.append(tempF, conditionIcon)
+        tempContainer.append(conditionContainer, condition)
+        forecastContainer.append(tempContainer, secondaryData);
+        container.append(location, localtime, forecastContainer);
+    }
+
+    function createSecondaryDataC(data) {
+        const secondaryData = document.createElement('div');
+        const windContainer = document.createElement('div');
+        const visibilityContainer = document.createElement('div');
+        const humidityContainer = document.createElement('div');
+        const uvContainer = document.createElement('div');
+        const cloudinessContainer = document.createElement('div');
+        const pressureContainer = document.createElement('div'); 
+        const wind = document.createElement('p');
+        const visibility = document.createElement('p');
+        const humidity = document.createElement('p');
+        const uv = document.createElement('p');
+        const cloudiness = document.createElement('p');
+        const pressure = document.createElement('p');
+    
+        wind.textContent = `${data.current.wind_kph} kph`;
+        visibility.textContent = `${data.current.vis_km} km.`;
+        humidity.textContent = `${data.current.humidity} %`;
+        uv.textContent = data.current.uv;
+        cloudiness.textContent = `${data.current.cloud} %`;
+        pressure.textContent = `${data.current.pressure_mb} mb.`;
+    
+        windContainer.textContent = 'Wind KPH';
+        windContainer.append(wind);
+        visibilityContainer.textContent = 'Visibility';
+        visibilityContainer.append(visibility);
+        humidityContainer.textContent = 'Humidity';
+        humidityContainer.append(humidity);
+        uvContainer.textContent = 'UV Index'
+        uvContainer.append(uv);
+        cloudinessContainer.textContent = 'Cloudiness';
+        cloudinessContainer.append(cloudiness);
+        pressureContainer.textContent = 'Pressure';
+        pressureContainer.append(pressure);
+    
+        secondaryData.className = 'secondary-data'
+    
+        secondaryData.append(
+            windContainer,
+            visibilityContainer,
+            humidityContainer,
+            uvContainer,
+            cloudinessContainer,
+            pressureContainer
+        )
+    
+        return secondaryData
+    }
+
+    function createThreeDayElementC(data) {
+        const threeDaysData = data.forecast.forecastday;
+        const threeDayContainer = document.createElement('div');
+        const threeDayWrapper = document.createElement('div');
+        const threeDayHeading = document.createElement('h1');
+
+        threeDaysData.forEach(day => {
+            const dayForecast = createDayElementC(day.date, day.day)
+            threeDayWrapper.append(dayForecast)
+        })
+
+        threeDayHeading.textContent = '3-Day Forecast'
+
+        threeDayWrapper.className = 'three-day-wrapper'
+
+        threeDayContainer.append(threeDayHeading, threeDayWrapper);
+        container.append(threeDayContainer);
+    }
+
+    function createDayElementC(date, dayData) {
+        const dayContainer = document.createElement('div');
+        const dayDate = document.createElement('h3');
+        const conditionText = document.createElement('p');
+        const conditionIcon = document.createElement('img');
+        const hiTemp = document.createElement('p');
+        const loTemp = document.createElement('p');
+
+        dayDate.textContent = date;
+        conditionText.textContent = dayData.condition.text;
+        conditionIcon.src = dayData.condition.icon;
+        loTemp.textContent = `Low: ${dayData.mintemp_c} \u00B0C`;
+        hiTemp.textContent = `High: ${dayData.maxtemp_c} \u00B0C`;
+
+        dayContainer.append(dayDate, conditionText, conditionIcon, loTemp, hiTemp);
+        
+        return dayContainer
+    }
+
     
     function showError(error) {
         container.innerHTML = '';
@@ -145,7 +262,15 @@ const dom = (() => {
         loader.display = 'none'
     }
 
-    return {createForecastElementF, createThreeDayElementF, showError, showLoader, hideLoader}
+    return {
+        createForecastElementF, 
+        createThreeDayElementF,
+        createForecastElementC,
+        createThreeDayElementC,
+        showError, 
+        showLoader, 
+        hideLoader
+    }
 })();
 
 export { dom }
--- src/index.js
+++ src/index.js
@@ -3,21 +3,63 @@ import { dom } from "./dom";
 
 const btn = document.querySelector('button');
 const searchBar = document.getElementById('search');
+const fBtn = document.getElementById('switchF');
+const cBtn = document.getElementById('switchC');
+let selectedMeasurement = 'F';
 
 async function getForecast() {
     const searchParams = searchBar.value;
     const url = `http://api.weatherapi.com/v1/forecast.json?key=e462c7619cbe421f908144015230606&q=${searchParams}&days=3`;
 
     try {
-        dom.showLoader();
-        const forecast = await fetch(url, {mode:'cors'});
-        const forecastData = await forecast.json();
-        dom.createForecastElementF(forecastData);
-        dom.createThreeDayElementF(forecastData);
-        dom.hideLoader();
+        switch(selectedMeasurement) {
+            case 'F':
+                fResults(url);
+                break;
+            case 'C':
+                cResults(url);
+                break;
+        }
     } catch (error) {
         dom.showError(error);
     }
 }
 
+async function fResults(url) {
+    dom.showLoader();
+    const forecast = await fetch(url, {mode:'cors'});
+    const forecastData = await forecast.json();
+    dom.createForecastElementF(forecastData);
+    dom.createThreeDayElementF(forecastData);
+    dom.hideLoader();
+}
+
+async function cResults(url) {
+    dom.showLoader();
+    const forecast = await fetch(url, {mode:'cors'});
+    const forecastData = await forecast.json();
+    dom.createForecastElementC(forecastData);
+    dom.createThreeDayElementC(forecastData);
+    dom.hideLoader();
+}
+
+function selectF() {
+    fBtn.style.border = 'white solid 2px';
+    cBtn.style.border = 'none';
+    selectedMeasurement = 'F';
+}
+
+function selectC() {
+    cBtn.style.border = 'white solid 2px';
+    fBtn.style.border = 'none';
+    selectedMeasurement = 'C';
+}
+
 btn.addEventListener('click', getForecast);
+fBtn.addEventListener('click', () => {
+    selectF();
+})
+
+cBtn.addEventListener('click', () => {
+    selectC();
+})
